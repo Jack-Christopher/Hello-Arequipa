e2bf6daa3ac69035cc5025ebaa9cc5acf62dec9d
Jack Christopher <jhuaihuah@unsa.edu.pe>
2022-12-24 22:44:30 -0500

    feat: final version of prototype

---
+++ constants/Images.js
@@ -0,0 +1,41 @@
+// local imgs
+const Onboarding = require("../assets/imgs/bg.png");
+const Logo = require("../assets/imgs/argon-logo.png");
+const LogoOnboarding = require("../assets/imgs/argon-logo-onboarding.png");
+const ProfileBackground = require("../assets/imgs/profile-screen-bg.png");
+const RegisterBackground = require("../assets/imgs/register-bg.png");
+const Pro = require("../assets/imgs/getPro-bg.png");
+const ArgonLogo = require("../assets/imgs/argonlogo.png");
+const iOSLogo = require("../assets/imgs/ios.png");
+const androidLogo = require("../assets/imgs/android.png");
+// internet imgs
+
+const ProfilePicture = 'https://images.unsplash.com/photo-1492633423870-w?fit=crop&w=1650&q=80';
+
+const Viewed = [
+  'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQVfxqBjycr1Ap2B50Yh0ikEnHtDPEhiqM-Xw&usqp=CAU',
+  'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQuCd-m7iQF1pBft6SEEeMl7zQTJBPQCA8vSg&usqp=CAU',
+  'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQONgXVLE2CYM3wf7zjbJm85a3UmdgqHQQ6WQ&usqp=CAU',
+  'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ37e_c9w0H1dsMkqV9VXpt-Jq40WJRzpZADw&usqp=CAU',
+  'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQPilj7aKyA1MHigumgEY1anQdavg_-doa8Mg&usqp=CAU',
+  'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT2gMG-nL5GrKguKPlJi_QnOkMbFoo0QLwUSg&usqp=CAU',
+];
+
+const Products = {
+  'View article': 'https://blog.airbaltic.com/wp-content/uploads/2020/01/header-souvenirs.jpg',
+};
+
+export default {
+  Onboarding,
+  Logo,
+  LogoOnboarding,
+  ProfileBackground,
+  ProfilePicture,
+  RegisterBackground,
+  Viewed,
+  Products,
+  Pro,
+  ArgonLogo,
+  iOSLogo,
+  androidLogo
+};
---
+++ constants/articles.js
@@ -0,0 +1,29 @@
+export default [
+  {
+    title: 'Monasterio de Santa Catalina',
+    image: 'https://santacatalina.org.pe/wp-content/uploads/2020/06/santa-catalina-home.jpg',
+    cta: 'Ver Sitio Turistico', 
+    horizontal: true
+  },
+  {
+    title: 'Plaza de Armas de Arequipa',
+    image: 'https://media.tacdn.com/media/attractions-splice-spp-674x446/0b/27/79/08.jpg',
+    cta: 'Ver Sitio Turistico',
+  },
+  {
+    title: 'Mirador de Yanahuara',
+    image: 'https://sites.google.com/site/lugaresturisticosaqpmjosemayca/_/rsrc/1442884455184/mirrador-de-yanahuara/arequipa%203.jpg?height=192&width=320',
+    cta: 'Ver Sitio Turistico',
+  },
+  {
+    title: 'Cañón del Colca',
+    image: 'https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcSO7E1KnwWzycsNGRb16xNYp8z_xC9G1M0hH8YN9bx7a8Rf0KZs0TbIgEgVYYBxXBjBUYqKnjsz6HrUd5DssIRX6A',
+    cta: 'Ver Sitio Turistico',
+  },
+  {
+    title: 'Claustro de la Compañía',
+    image: 'https://encrypted-tbn0.gstatic.com/licensed-image?q=tbn:ANd9GcRPbQHkA4TJ__BlMoFtsFBPzpGnB_Rt6dQDbiBZ56fEQmxS9epPEzT77_pHfvQE7usieGvzc_FRDAdanpG2gJaAYA',
+    cta: 'Ver Sitio Turistico',
+    horizontal: true
+  },
+];
